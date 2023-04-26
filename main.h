7422148620994ebbf3cc189fcea01c8067dec5b4
creationdis <creationdis@gmail.com>
2023-04-26 16:58:17 -0700

    task 0

--- main.h
+++ main.h
@@ -7,40 +7,36 @@
 #include <unistd.h>
 #include <limits.h>
 
-#define UNUSED(x) (void)(x)
 #define BUFF_SIZE 1024
 
-
-
-/* print name of function */
+typedef struct flags {
+    unsigned int plus;
+    unsigned int space;
+    unsigned int hashtag;
+    unsigned int h;
+    unsigned int l;
+    unsigned int width;
+    unsigned int precision;
+    unsigned int zero;
+    unsigned int minus;
+} flags_t;
 
 int _putchar(char c);
 int _printf(const char *format, ...);
-int handle_conversion_specifier(const char **p, va_list args);
-int print_char(va_list args);
-int print_string(va_list args);
-int print_percent(va_list args);
-int print_int(va_list args);
-int print_binary(va_list args);
-int print_unsigned(va_list args);
-int print_octal(va_list args);
-int print_hexadecimal(va_list args);
-int print_hexa_upper(va_list args);
-int print_S(va_list args);
-int print_address(va_list args);
-int print_rev(va_list args);
-int print_rot13(va_list args);
-
-/* flag   Width   .precision */
-
-unsigned int plus_flag;
-unsigned int space_flag;
-unsigned int hashtag_flag;
-unsigned int h;
-unsigned int l;
-unsigned int width;
-unsigned int precision;
-unsigned int zero_flag;
-unsigned int minus_flag;
+int handle_conversion_specifier(const char **p, va_list args, flags_t *flags);
+int print_char(va_list args, flags_t *flags);
+int print_string(va_list args, flags_t *flags);
+int print_percent(va_list args, flags_t *flags);
+int print_int(va_list args, flags_t *flags);
+int print_binary(va_list args, flags_t *flags);
+int print_unsigned(va_list args, flags_t *flags);
+int print_octal(va_list args, flags_t *flags);
+int print_hexadecimal(va_list args, flags_t *flags);
+int print_hexa_upper(va_list args, flags_t *flags);
+int print_S(va_list args, flags_t *flags);
+int print_address(va_list args, flags_t *flags);
+int print_rev(va_list args, flags_t *flags);
+int print_rot13(va_list args, flags_t *flags);
 
 #endif /* MAIN_H */
+
--- printf.c
+++ printf.c
@@ -17,72 +17,106 @@
  * end output to strings).
  * If an output error is encountered, a negative value is returned.
  */
+
 int _printf(const char *format, ...)
 {
-	va_list args;
-	const char *p = format;
-	int count = 0;
+    va_list args;
+    const char *p = format;
+    int count = 0;
 
-	if (format == NULL)
-		return (-1);
+    if (format == NULL)
+        return (-1);
 
-	va_start(args, format);
+    va_start(args, format);
 
-	while (*p != '\0')
-	{
-		if (*p == '%')
-		{
-			int printed = handle_conversion_specifier(&p, args);
-			if (printed < 0)
-				return (-1);
-			count += printed;
-		}
-		else
-		{
-			putchar(*p);
-			count++;
-		}
-		p++;
-	}
+    while (*p != '\0')
+    {
+        if (*p == '%')
+        {
+            p++;
+            flags_t flags = {0};
+            while (*p != '\0' && is_flag(*p))
+            {
+                set_flag(*p, &flags);
+                p++;
+            }
+            handle_conversion_specifier(&p, args, &flags, &count);
+        }
+        else
+        {
+            _putchar(*p);
+            count++;
+        }
+        p++;
+    }
 
-	va_end(args);
+    va_end(args);
 
-	return (count);
+    return (count);
 }
+
 
 /**
  * handle_conversion_specifier - Handles a conversion specifier.
  *
  * @p: A pointer to a pointer to a character in the format string.
  * @args: A va_list of arguments to print.
+ * @flags: A pointer to a struct containing the flag settings.
  *
  * Return: The number of characters printed for this specifier, or -1 on failure.
  */
-int handle_conversion_specifier(const char **p, va_list args)
+
+int handle_conversion_specifier(const char **p, va_list args, flags_t *flags, int *count)
 {
-	int count = 0;
-
-	/* Skip over the initial '%' */
-	(*p)++;
-
-	switch (**p)
-	{
-	case 'c':
-		count += putchar(va_arg(args, int));
-		break;
-	case 's':
-		count += printf("%s", va_arg(args, char *));
-		break;
-	case '%':
-		count += putchar('%');
-		break;
-	default:
-		putchar('%');
-		putchar(**p);
-		count += 2;
-		break;
-	}
-
-	return (count);
+    int printed = 0;
+    switch (**p)
+    {
+        case 'c':
+            printed = print_char(args, flags);
+            break;
+        case 's':
+            printed = print_string(args, flags);
+            break;
+        case '%':
+            printed = print_percent(args, flags);
+            break;
+        case 'd':
+        case 'i':
+            printed = print_int(args, flags);
+            break;
+        case 'b':
+            printed = print_binary(args, flags);
+            break;
+        case 'u':
+            printed = print_unsigned(args, flags);
+            break;
+        case 'o':
+            printed = print_octal(args, flags);
+            break;
+        case 'x':
+            printed = print_hexadecimal(args, flags);
+            break;
+        case 'X':
+            printed = print_hexa_upper(args, flags);
+            break;
+        case 'S':
+            printed = print_S(args, flags);
+            break;
+        case 'p':
+            printed = print_address(args, flags);
+            break;
+        case 'r':
+            printed = print_rev(args, flags);
+            break;
+        case 'R':
+            printed = print_rot13(args, flags);
+            break;
+        default:
+            _putchar('%');
+            _putchar(**p);
+            *count += 2;
+            break;
+    }
+    return printed;
 }
 
