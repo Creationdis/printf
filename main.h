7e873441ebb12f2171672c933edb7e162cf1854c
creationdis <creationdis@gmail.com>
2023-04-26 08:56:53 -0700

    task 0

--- main.h
+++ main.h
@@ -14,31 +14,21 @@
 
 /* print name of function */
 
-int _puts(char *str);
-int _printf(const char *format, ...);
 int _putchar(char c);
-int print_char(va_list types, char buffer[],
-	int flags, int width, int precision, int size);
-int print_string(va_list types, char buffer[],
-	int flags, int width, int precision, int size);
-int print_percent(va_list types, char buffer[],
-	int flags, int width, int precision, int size);
-/* Functions to print numbers */
-int print_int(va_list types, char buffer[],
-	int flags, int width, int precision, int size);
-int print_binary(va_list types, char buffer[],
-	int flags, int width, int precision, int size);
-int print_unsigned(va_list types, char buffer[],
-	int flags, int width, int precision, int size);
-int print_octal(va_list types, char buffer[],
-	int flags, int width, int precision, int size);
-int print_hexadecimal(va_list types, char buffer[],
-	int flags, int width, int precision, int size);
-int print_hexa_upper(va_list types, char buffer[],
-	int flags, int width, int precision, int size);
-
-int print_hexa(va_list types, char map_to[],
-char buffer[], int flags, char flag_ch, int width, int precision, int size);
+int _printf(const char *format, ...);
+int print_char(va_list args);
+int print_string(va_list args);
+int print_percent(va_list args);
+int print_int(va_list args);
+int print_binary(va_list args);
+int print_unsigned(va_list args);
+int print_octal(va_list args);
+int print_hexadecimal(va_list args);
+int print_hexa_upper(va_list args);
+int print_S(va_list args);
+int print_address(va_list args)
+int print_rev(va_list args);
+int print_rot13(va_list args);
 
 /* flag   Width   .precision */
 
